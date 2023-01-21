02238295ed26806c7ff755c2fa9bd01a57a86b60
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-01-21 15:24:37 -0800

    the devmwang is very demanding

--- src/components/navbaritem.tsx
+++ src/components/navbaritem.tsx
@@ -1,20 +1,11 @@
 import Link from "next/link";
 
-export default function Layout({ input, url, svg_path }: { input: string; url: string; svg_path: string }) {
+export default function Layout({ input, url, svg }: { input: string; url: string; svg: any }) {
     return (
         <Link href={url}>
-            <div className="grid place-items-center bg-slate-800 px-6 py-6 shadow-xl">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="white"
-                    className="h-6 w-6"
-                >
-                    <path strokeLinecap="round" strokeLinejoin="round" d={svg_path} />
-                </svg>
-                <h3 className="mt-5 text-base font-medium tracking-tight text-white">{input}</h3>
+            <div className="grid place-items-center bg-slate-800 px-2 py-2 shadow-xl">
+                {svg}
+                <h3 className="mt-2 text-base font-medium tracking-tight text-white">{input}</h3>
             </div>
         </Link>
     );
