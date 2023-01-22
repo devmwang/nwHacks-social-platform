8df449aa616e280ec99f84cd78e8557f9d357a37
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-01-21 16:13:45 -0800

    thingy

--- src/components/navbaritem.tsx
+++ src/components/navbaritem.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Layout({ input, url, svg }: { input: string; url: string; svg: any }) {
+export default function Layout({ input, url, svg }: { input: string; url: string; svg: JSX.Element }) {
     return (
         <Link href={url}>
             <div className="grid place-items-center rounded-lg bg-slate-800 px-2 py-2 shadow-xl hover:bg-white/20">
