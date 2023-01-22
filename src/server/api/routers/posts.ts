e4c4ca25f062892017f56409df494bb067bd5ce9
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-01-21 21:25:13 -0800

    Change proc type in posts router

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,13 +1,12 @@
 import { z } from "zod";
-import aws from "aws-sdk";
 
-import { createTRPCRouter, publicProcedure, protectedProcedure, organizationProtectedProcedure } from "../trpc";
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 import { env } from "@env/server.mjs";
 
 export const postsRouter = createTRPCRouter({
-    hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+    hello: publicProcedure.input(z.object({ text: z.any() })).query(({ input }) => {
         return {
-            greeting: `Hello ${input.text}`,
+            greeting: `Hello ${input.text as string}`,
         };
     }),
 
@@ -15,32 +14,11 @@ export const postsRouter = createTRPCRouter({
         return "you can now see this secret message!";
     }),
 
-    createPost: organizationProtectedProcedure
-        .input(z.object({ image: z.any(), caption: z.string() }))
-        .mutation(({ input }) => {
-            const s3 = new aws.S3({
-                accessKeyId: env.BB_ID,
-                secretAccessKey: env.BB_KEY,
-                endpoint: env.BB_URL,
-                region: "us-west-004",
-            });
+    createPost: protectedProcedure.input(z.object({ image: z.any(), caption: z.string() })).mutation(({ input }) => {
+        console.log("b");
 
-            aws.config.update({
-                accessKeyId: env.BB_ID,
-                secretAccessKey: env.BB_KEY,
-                region: "us-west-004",
-            });
-
-            const post = s3.createPresignedPost({
-                Bucket: "nwHacksSocialPlatform",
-                Fields: {
-                    key: input.image as FileList,
-                },
-                Expires: 60,
-            });
-
-            return {
-                status: "success",
-            };
-        }),
+        return {
+            status: "success",
+        };
+    }),
 });
